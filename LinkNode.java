5c122b7b913ed8981307128de524d58483538e28
Mikedan37 <mn_danylchuk@me.com>
2022-02-10 16:42:55 -0800

    Update LinkNode.java

--- LinkNode.java
+++ LinkNode.java
@@ -1,5 +1,19 @@
 package lab3;
-
-public class LinkNode {
-
+/*   
+ *  Name Block:
+ *  Lab Number: Lab 3
+ *  Name: Michael Danylchuk, Maxim Manokhin
+ *  Team: 1
+ *  The purpose of this assignment is to demonstrate our understanding of polymorphism, encapsulation
+ *  and classes by creating a currency simulator that can add compare and manipulate objects
+ */
+abstract class LinkNode extends Currency {
+	Currency data;
+	LinkNode next;
+	
+	private LinkNode(Currency data) {
+		this.data = data;
+		this.next = null;
+	}
+	
 }
