72d114cfd0fcd6bc77406e0641db335ba73f7f2b
Mikedan37 <mn_danylchuk@me.com>
2022-02-13 03:38:16 -0800

    Update Pound.java

--- Pound.java
+++ Pound.java
@@ -1,66 +1,78 @@
-
 /*   
  *  Name Block:
- *  Lab Number: Lab 2
+ *  Lab Number: Lab 3
  *  Name: Michael Danylchuk, Maxim Manokhin
  *  Team: 1
  *  The purpose of this assignment is to demonstrate our understanding of polymorphism, encapsulation
  *  and classes by creating a currency simulator that can add compare and manipulate objects
  */
 
-/*
- *  Things that were changed: Add, Subtract, IsGreater, IsEqual, method was removed an only in Currency class to utalize,
- *  and display polymorphism. Creatend empty abstract Methods
- */
+//public class Pound extends Currency{
+//	private String currencyName;
+//	private String coinName;
+//	
+//	public Pound() {
+//		super();
+//		this.currencyName = "Pound";
+//		this.coinName = "Cent";
+//	}
+//	
+//	public Pound(int noteValue,int coinValue) {
+//		super(noteValue, coinValue);
+//		this.currencyName = "Pound";
+//		this.coinName = "Cent";
+//	}
+//	
+//	@Override
+//	public Currency add(Currency currency) {
+//		Pound other = (Pound) currency;
+//		int notes = super.getNoteValue() + other.getCoinValue();
+//		int coins = super.getCoinValue() + other.getCoinValue();
+//		if(coins >= 100) {
+//			notes++ ;
+//			coins -= 100;
+//		}
+//		return new Pound(notes,coins);
+//	}
+//	@Override
+//	public Currency subtract(Currency currency) {
+//		Pound other = (Pound) currency;
+//		int notes = super.getNoteValue() - other.getNoteValue();
+//		int coins = super.getCoinValue() - other.getNoteValue();
+//		if(coins < 0) {
+//			notes--;
+//			coins += 100;
+//		}
+//		return new Dollar(noteValue,coinValue);
+//	}
+//
+//	@Override
+//	public int compare(Currency currency) {
+//		Pound other = (Pound) currency;
+//		if(this.getNoteValue() > other.getNoteValue()) {
+//			return 1;
+//		}else if(this.getNoteValue() < other.getNoteValue()) {
+//			return -1;
+//		}else if(this.getCoinValue() > other.getCoinValue()) {
+//			return 1;
+//		}else if(this.getCoinValue() < other.getCoinValue()) {
+//			return -1;
+//		} else {
+//			return 0;
+//		}
+//		
+//	}
+//
+//	@Override
+//	public String printCurrency() {
+//		String str = new String();
+//		if(super.getCoinValue() > 10) {
+//			str = super.getNoteValue() + " " + currencyName + " " + super.getCoinValue() + " " + coinName;
+//		}else {
+//			str = super.getNoteValue() + ".0" + currencyName + " " + super.getCoinValue() + " " + coinName;
+//		}	
+//	    return str;
+//	}
+//
+//}
 
-public class Pound extends Currency{
-// Non public String Attribute which contains name of Pound 
-private String type = "Pound";
-	
-    //Dont need pre or post documentation for constructors or destructors
-	public Pound(double d) {
-	// TODO Auto-generated constructor stub
-	}
-	
-	public Pound() {
-	// Auto generated	
-	}
-
-	/*
-	 * The purpose of these two methods is to be a getter for the String attribute of this class
-	 * Pre: No input
-	 * Post: returns type variable with name of pound
-	 */
-	
-	public String getType() {
-		return type;
-	}
-	
-	/*
-         * The purpose of this method is to print out the name and components of the currency
-         * Pre: No input
-         * Post: prints unit and fractional values of Pound object and name pound
-         */
-	
-	public void print() {
-		System.out.print(this.getUnit()+"."+this.getFraction()+" "+ type + "\n");
-	}
-	
-	@Override
-	protected lab2.Currency Currency(lab2.Currency x) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	protected lab2.Currency Pound(lab2.Currency a) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	protected lab2.Currency Dollar(lab2.Currency a) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-}
