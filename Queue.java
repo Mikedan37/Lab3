c8fd2ef27637964734f18d5f216edd5f0c74ae1b
Mikedan37 <mn_danylchuk@me.com>
2022-02-13 03:37:20 -0800

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -1,5 +1,38 @@
-package lab3;
+/*   
+ *  Name Block:
+ *  Lab Number: Lab 3
+ *  Name: Michael Danylchuk, Maxim Manokhin
+ *  Team: 1
+ *  The purpose of this assignment is to demonstrate our understanding of polymorphism, encapsulation
+ *  and classes by creating a currency simulator that can add compare and manipulate objects
+ */
 
-public class Queue {
-
+public class Queue extends SingleLinkedList{
+	
+	public Queue() {
+		super();
+	}
+	
+	public void enqueue(LinkNode nd) {
+		super.add(nd);
+	}
+	
+	public void dequeue() {
+		super.delete(super.head);
+	}
+	
+	public LinkNode peekFront(){
+		if(super.length() == 0){
+			System.out.println("The queue is empty"); System.exit(1);
+		}	
+			return super.head;	
+	}
+	
+	public LinkNode peekRear() {
+		if(super.length() == 0){
+			System.out.println("The queue is empty"); System.exit(1);
+		}
+			return super.end;
+		}
 }
+
